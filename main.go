67e4002a7a61df93fb174e05ee383de6930870dd
Klithik <santi17461@gmail.com>
2024-07-09 11:27:11 -0400

    funcionando ping para is-alive

--- main.go
+++ main.go
@@ -3,38 +3,26 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"net/http"
 	"os"
-    "time"
+    "net/http"
 ) 
 
 func main() {
-    //mux := http.NewServeMux()
     fmt.Println("Introduce una dirección objetivo:")
     scanner := bufio.NewReader(os.Stdin)
     target, _ := scanner.ReadString('\n')    
-    isAlive := pingURL2(target)
-    fmt.Printf("Funciona %v\n",isAlive)
+    target = target[:len(target)-1] 
+
+    resp := Ping(target)
+    fmt.Printf("is-alive: %t",resp)
 }
 
-func pingURL(target string) bool {
-    client := http.Client{
-        Timeout: 5 * time.Second,
-    }
-    resp, err := client.Get(target)
-    if err != nil {
-        return false
-    }
-    defer resp.Body.Close()
-    return resp.StatusCode == http.StatusOK
-}
-
-func pingURL2(url string) (bool) {
+func Ping(url string) (bool){
 	resp, err := http.Get(url)
 	if err != nil {
+		fmt.Printf("Error al hacer la solicitud: %v\n", err)
 		return false
 	}
 	defer resp.Body.Close()
-
-	return true 
+    return true
 }
